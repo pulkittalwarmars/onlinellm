70eebc8318a8b6b14dd19229300c81283ccdde97
Pulkit Talwar <pulkittalwar8@gmail.com>
2024-06-28 13:09:22 -0400

    added the basic version of the api which has no web capabilities

--- src/app.py
+++ src/app.py
@@ -3,20 +3,26 @@
 ## Will delete this for the final version
 
 import os
+import sys
 from dotenv import load_dotenv
-from openai import OpenAI
+from openai import AzureOpenAI
+from src.exception import CustomException
 
 
 
-client = OpenAI(base_url=<DEPLOYMENT_ENDPOINT>,
-   api_key=<API_KEY_FOR_DEPLOYMENT>)
+client = AzureOpenAI(azure_endpoint=os.getenv("AZURE_ENDPOINT"),
+                     api_version="2024-02-01",
+                     api_key=os.getenv("OPENAI_API_KEY"))
 
-   completion = client.chat.completions.create(
-     model="<ONLINE_MODEL_ID>",
-     messages=[
-       {"role": "system", "content": "You are a helpful assistant."},
-       {"role": "user", "content": "What is the top news about Gen AI today? "}
-     ]
-)
+try:
+  completion = client.chat.completions.create(
+      model="pt_rekoncile",
+      messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is the top news about Gen AI today? "}
+      ]
+  )
 
-print(completion.choices[0].message)
+  print(completion.choices[0].message)
+except Exception as e:
+  raise CustomException(e,sys)
